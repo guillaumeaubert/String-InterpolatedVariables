a23cea0ce9511b32b3d29e28fc9d34c95ee6084e
Guillaume Aubert <aubertg@cpan.org>
2014-04-17 18:22:17 -0700

    Released version 1.0.3.

--- lib/String/InterpolatedVariables.pm
+++ lib/String/InterpolatedVariables.pm
@@ -13,11 +13,11 @@ String::InterpolatedVariables - Extract variable names from interpolated strings
 
 =head1 VERSION
 
-Version 1.0.2
+Version 1.0.3
 
 =cut
 
-our $VERSION = '1.0.2';
+our $VERSION = '1.0.3';
 
 
 =head1 SYNOPSIS
